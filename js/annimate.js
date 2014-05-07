f6cb64b28be6701b0aebc65e777f6fc4dffe394a
ETagu <tagukeiosfc@gmail.com>
2014-05-07 16:00:14 +0900

    説明の追加

--- js/annimate.js
+++ js/annimate.js
@@ -4,6 +4,8 @@ var endFlipFoxkeh = function(){
 
 	foxkeh.setAttribute("class", "");
 };
+//foxkehのフリップの終了について
+//slidein-foxkeh内のimgをここで選択
 
 var endSlideinFoxkehAndStartFlipFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
@@ -13,6 +15,9 @@ var endSlideinFoxkehAndStartFlipFoxkeh = function(){
 
 	foxkeh.setAttribute("class", "flip");
 };
+//foxkehのアニメーションの始まりと終わりの動きについて
+//slidein-foxkeh内のimgを選択
+
 
 var startSlideinFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
@@ -20,6 +25,10 @@ var startSlideinFoxkeh = function(){
 
 	foxkeh.addEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
 };
-
+//slidein-foxkeh内のimgにfoxkehという名前を付ける。
+//foxkehのclass属性にslideinを追加。
+//foxkehに”animationend"というイベントが起きたらendslideinFoxkehAndStartFlipFoxehを行う。
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
 slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
+//slideinFoxehButtonという名前をindex.html内に定義する。
+//slideinFoxehButtonに"click"というイベントが起きたらstartSlideinFoxkehを行う。
--- js/slidein.js
+++ js/slidein.js
@@ -3,12 +3,15 @@ var endSlideinFoxkeh = function(){
 <<<<<<< HEAD
 	foxkeh.setAttribute("class", "slidein");
 };
+//スライドを終了させるためのテキスト
+//slidein-foxkeh imgからfoxkehを選択
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
 slideinFoxkehButton.addEventListener("mouseover", slideinFoxkeh);
 =======
 	foxkeh.setAttribute("class", "");
 };
+//mouseoverという動きでslideinfoxkehが発動するという動きを追加
 
 var startSlideinFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
@@ -16,7 +19,10 @@ var startSlideinFoxkeh = function(){
 
 	foxkeh.addEventListener("animationend", endSlideinFoxkeh);
 };
+//foxkehのスライドのアニメーションの始まりについて
+//endSlideinFoxkehの一連の動きを追加
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
 slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
 >>>>>>> origin/make_slidein_retriable
+//clickという動作をするとstartslideinfoxkehが発動するというイベントを追加
